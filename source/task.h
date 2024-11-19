7c98611515f7c635e9c0190f462ea9b4584c6d70
vityaman <vityaman.dev@yandex.ru>
2024-11-19 10:00:31 +0300

    cleanup

--- source/task.h
+++ source/task.h
@@ -7,18 +7,18 @@ struct task* task_current();
 void* unsafe_task_argument(struct task* task);
 void unsafe_task_exit();
 
-#define DECLARE_TASK(name, type, argument) \
-  void task_body_##name(type* argument);   \
+#define DECLARE_TASK(name, type, argument)            \
+  void task_body_##name(type* argument); /* NOLINT */ \
   void name()
 
-#define DEFINE_TASK(name, type, argument)                   \
-  DECLARE_TASK(name, type, argument);                       \
-                                                            \
-  void name() {                                             \
-    struct task* self = task_current();                     \
-    type* __argument = (type*)(unsafe_task_argument(self)); \
-    task_body_##name(__argument);                           \
-    unsafe_task_exit();                                            \
-  }                                                         \
-                                                            \
-  void task_body_##name(type* argument)
+#define DEFINE_TASK(name, type, argument)                                \
+  DECLARE_TASK(name, type, argument);                                    \
+                                                                         \
+  void name() {                                                          \
+    struct task* self = task_current();                                  \
+    type* __argument = (type*)(unsafe_task_argument(self)); /* NOLINT */ \
+    task_body_##name(__argument);                                        \
+    unsafe_task_exit();                                                  \
+  }                                                                      \
+                                                                         \
+  void task_body_##name(type* argument) /* NOLINT */
