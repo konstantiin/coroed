3d67862a94355d5fc1b5c392c2da3e3c4b0086c8
vityaman <vityaman.dev@yandex.ru>
2024-11-21 10:30:58 +0300

    better precommit

--- ci/precommit.bash
+++ ci/precommit.bash
@@ -4,6 +4,15 @@ set -e
 
 cd "$(dirname "$0")"/..
 
+echo "Fixing formatting..."
+make fix-format
+
+echo "Checking formatting..."
+make check-format
+
+echo "Checking style..."
+make check-style
+
 COMPILERS=("gcc" "clang")
 OPTIMIZATION_LEVELS=("-O0" "-O3")
 SANITIZERS=("" "address,leak")
@@ -23,9 +32,17 @@ for compiler in "${COMPILERS[@]}"; do
                 COMPILER="$compiler" \
                 OPTIMIZATION_LEVEL="$optimization_level" \
                 SANITIZERS="$SAN"
+
+            pids=()
             for i in {1..8}; do
-                echo "|- Running #$i..."
-                ./build/bin/app 1>/dev/null
+                ./build/bin/app 1>/dev/null &
+                pids[${i}]=$!
+                echo "|- Running #$i (pid: ${pids[$i]})..."
+            done
+
+            for i in {1..8}; do
+                wait ${pids[$i]}
+                echo "|- Done #$i (pid: ${pids[$i]})."
             done
         done
     done
